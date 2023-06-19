138f0700b630f004a3f5b2e7250de41908ee4b53
adrablezzz <1942775049@qq.com>
2023-06-20 00:49:14 +0800

    c

--- web_spider/2.book_spider.py
+++ web_spider/2.book_spider.py
@@ -1,35 +1,35 @@
 from tool import http_request
+from tool import http_request_post
 from tool import my_filter
 from tool import my_map
-import time
+import time, sys
+from bs4 import BeautifulSoup
 
 # 笔趣阁
 class Downloader:
-    base_url = ''
-    book_path = ''
     book_url = ''
     book_name = ''
-    out_path = ''
     directorys = []
     rewrite_flag = True
     search_list = []
 
-    def __init__(self, base_url, book_path, book_name='小说', out_path='./out') -> None:
-        self.base_url = base_url
-        self.book_path = book_path
-        self.book_url = '{}/{}'.format(base_url, book_path)
-        self.book_name = book_name
-        self.out_path = out_path
+    def __init__(self) -> None:
+        pass
     
     def start_download(self):
+        print('《{}》请求中...'.format(self.book_name))
         self.get_book_directory()
-        for i in range(len(self.directorys)):
+        print('《{}》开始下载'.format(self.book_name))
+        length = len(self.directorys)
+        for i in range(length):
+            sys.stdout.write('  下载中：{:.2%}\r'.format(i/length))
+            sys.stdout.flush()
+            time.sleep(0.5) # 防止服务器反dos机制关闭连接
+
             item = self.directorys[i]
             item_content = self.get_book_chapter(self.book_url + '/' + item['url'])
             self.writer(item_content)
-            print('下载中：{:.2%}'.format((i+1)/len(self.directorys)))
-            time.sleep(0.5) # 防止服务器反dos机制关闭连接
-        print('下载完成')
+        print('《{}》下载完成：100%'.format(self.book_name))
 
     # 获取章节内容
     def get_book_chapter(self, url):
@@ -55,34 +55,65 @@ class Downloader:
                 index = i
             obj = {'title': item.string or '', 'url': item.get('href') or ''}
             res.append(obj)
-        return res[index+1:]
+        # return res[index+1:]
+        return res[index+1:15]
 
     def writer(self, text=''):
         if(self.rewrite_flag):
-            open('{}/{}.txt'.format(self.out_path, self.book_name), 'w').close()
+            open('{}.txt'.format(self.book_name), 'w').close()
             self.rewrite_flag = False
 
-        with open('{}/{}.txt'.format(self.out_path, self.book_name), 'a', encoding='utf-8') as f:
+        with open('{}.txt'.format(self.book_name), 'a', encoding='utf-8') as f:
             f.writelines(text)
             f.write('\n\n')
 
     # 搜索小说 需要绕过反爬虫验证
-    def search_book(self, book_name):
-        search_url = 'https://www.biqudd.com/modules/article/search.php?searchkey=%C8%FD%CC%E5'
+    def search_book(self, search_key=''):
+        search_url = 'https://www.biqudd.com/modules/article/search.php?searchkey=' + search_key
         response = http_request(search_url)
+        with open('error_page.txt', 'w', encoding='utf-8') as f:
+            f.writelines(str(response))
         book_list = response.select('.novelslistss a')
         def f(i):
-            return book_name in i.string
+            return search_key in i.string
         filtered_list = my_filter(book_list, f)
         def mf(i):
             return {'title': i.string, 'url': i.get('href')}
         self.search_list = my_map(filtered_list, mf)
+    
+    def search_book_post(self, search_key=''):
+        search_url = 'https://www.biqudd.com/modules/article/search.php'
+        response = http_request_post(search_url, data={'searchtype': 'articlename', 'searchkey': search_key.encode('gbk')})
+        with open('error_page.txt', 'w', encoding='utf-8') as f:
+            f.writelines(str(response))
+        book_list = response.select('.novelslistss li')
+        def mf(item):
+            i = BeautifulSoup(str(item), 'html.parser')
+            book_link = i.select('.s2 a')[0]
+            author_name = i.select('.s4')[0].string
+            return {'title': book_link.string, 'url': book_link.get('href'), 'author': author_name}
+        self.search_list = my_map(book_list, mf)
         print(self.search_list)
-        pass
 
 
-BASE_URL = 'https://www.biqudd.com'
-BOOK_PATH = '/86_86695'
-dl = Downloader(base_url=BASE_URL, book_path=BOOK_PATH, book_name='三体')
-# dl.start_download()
-dl.search_book('三体')
+    def set_downloader(self, book_name, book_url):
+        self.book_name = book_name
+        self.book_url = book_url
+
+
+if __name__ == '__main__':
+    BASE_URL = 'https://www.biqudd.com'
+    print('欢迎使用小说下载器！(书籍来源：{})'.format(BASE_URL))
+    search_key = input('请输入书名(宁缺勿多)：')
+    dl = Downloader()
+    dl.search_book_post(search_key)
+    if(len(dl.search_list) > 0):
+        list_str = ''
+        for i in range(len(dl.search_list)):
+            list_str += '{} - {} - {}\n'.format(i, dl.search_list[i]['title'], dl.search_list[i]['author'])
+        index = eval(input('搜索到以下书籍，请输入数字进行选择：\n' + list_str))
+        choice = dl.search_list[index*1]
+        dl.set_downloader(choice['title'], choice['url'])
+        dl.start_download()
+    else:
+        print('未搜索到相关书籍！')
--- web_spider/tool.py
+++ web_spider/tool.py
@@ -1,13 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
-# http请求
+# http get请求
+
 def http_request(url):
-    target = url
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0',
-        'Cookie': 'Hm_lvt_094775c050f5046091b59ab43cd68c32=1687019133,1687066316; Hm_lvt_a71b1bc761fe3f26085e79b5fd6a7f71=1687019133,1687066316; __gads=ID=bbc4c62b378eb33e-22cc6908a9b4005c:T=1687019134:RT=1687068298:S=ALNI_MYtU7E1FuIXyy8iWT5pFMwXqgQ_SQ; __gpi=UID=00000c50c84c561e:T=1687019134:RT=1687068298:S=ALNI_Ma_OTMWPllgZ9lWfG3_Ew2X97Y6Xg; Hm_lpvt_094775c050f5046091b59ab43cd68c32=1687100879; Hm_lpvt_a71b1bc761fe3f26085e79b5fd6a7f71=1687100879; cf_chl_2=bb7582c19847ce5; cf_clearance=5bUS.nWd7vI8UjpM1q7uZu7ecVp9NfDEL5zJpmorX.I-1687100188-0-160; jieqiVisitTime=jieqiArticlesearchTime%3D1687100879'
+        # 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0',
+        # 'Cookie': 'Hm_lvt_094775c050f5046091b59ab43cd68c32=1687019133,1687066316; Hm_lvt_a71b1bc761fe3f26085e79b5fd6a7f71=1687019133,1687066316; __gads=ID=bbc4c62b378eb33e-22cc6908a9b4005c:T=1687019134:RT=1687068298:S=ALNI_MYtU7E1FuIXyy8iWT5pFMwXqgQ_SQ; __gpi=UID=00000c50c84c561e:T=1687019134:RT=1687068298:S=ALNI_Ma_OTMWPllgZ9lWfG3_Ew2X97Y6Xg; Hm_lpvt_094775c050f5046091b59ab43cd68c32=1687100879; Hm_lpvt_a71b1bc761fe3f26085e79b5fd6a7f71=1687100879; cf_chl_2=bb7582c19847ce5; cf_clearance=5bUS.nWd7vI8UjpM1q7uZu7ecVp9NfDEL5zJpmorX.I-1687100188-0-160; jieqiVisitTime=jieqiArticlesearchTime%3D1687100879'
+
+        # 2023年6月19日22:22:33 chorme
+        # 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        # 'Cookie': 'Hm_lvt_094775c050f5046091b59ab43cd68c32=1687068391,1687182983; Hm_lvt_a71b1bc761fe3f26085e79b5fd6a7f71=1687068391,1687182983; cf_chl_2=92e173fa3dec8f0; cf_clearance=z_mNQKN06uZJHge9T7QyOIzC8.A9PlgR4STdbu8MxMo-1687183004-0-160; jieqiVisitId=article_articleviews%3D86693; __gads=ID=a485432064a7ee99-22a8a73bafe10077:T=1687068392:RT=1687183268:S=ALNI_MYSyj0mPUg7qdi_MyfckVec3BkmTQ; __gpi=UID=00000c5107fd5f03:T=1687068392:RT=1687183268:S=ALNI_MZ6HGkmFxDyIXOJQIQjOWI3eu3gdw; jieqiVisitTime=jieqiArticlesearchTime%3D1687184037; Hm_lpvt_094775c050f5046091b59ab43cd68c32=1687184038; Hm_lpvt_a71b1bc761fe3f26085e79b5fd6a7f71=1687184038',
+
+        # 2023年6月19日22:43:23
+        # 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        # 'Cookie': 'Hm_lvt_094775c050f5046091b59ab43cd68c32=1687068391,1687182983; Hm_lvt_a71b1bc761fe3f26085e79b5fd6a7f71=1687068391,1687182983; jieqiVisitId=article_articleviews%3D86693; __gads=ID=a485432064a7ee99-22a8a73bafe10077:T=1687068392:RT=1687183268:S=ALNI_MYSyj0mPUg7qdi_MyfckVec3BkmTQ; __gpi=UID=00000c5107fd5f03:T=1687068392:RT=1687183268:S=ALNI_MZ6HGkmFxDyIXOJQIQjOWI3eu3gdw; cf_chl_2=5489765364bb22c; cf_clearance=MRa_W5UVSTARGuwFnJiQiAAza1A8XHTcU_xcjz5ef4Y-1687185594-0-160; jieqiVisitTime=jieqiArticlesearchTime%3D1687185623; Hm_lpvt_094775c050f5046091b59ab43cd68c32=1687185625; Hm_lpvt_a71b1bc761fe3f26085e79b5fd6a7f71=1687185625'
+
+        # 2023年6月19日23:55:33
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        'Cookie': 'Hm_lvt_094775c050f5046091b59ab43cd68c32=1687068391,1687182983; Hm_lvt_a71b1bc761fe3f26085e79b5fd6a7f71=1687068391,1687182983; __gads=ID=a485432064a7ee99-22a8a73bafe10077:T=1687068392:RT=1687183268:S=ALNI_MYSyj0mPUg7qdi_MyfckVec3BkmTQ; __gpi=UID=00000c5107fd5f03:T=1687068392:RT=1687183268:S=ALNI_MZ6HGkmFxDyIXOJQIQjOWI3eu3gdw; cf_clearance=LhLt9juUAWpwP6yGygMBwFsI0olrUhoeWHZHKUqDy38-1687190062-0-160; jieqiVisitTime=jieqiArticlesearchTime%3D1687191280; Hm_lpvt_094775c050f5046091b59ab43cd68c32=1687191952; Hm_lpvt_a71b1bc761fe3f26085e79b5fd6a7f71=1687191952; cf_chl_2=6e792d39cf13f69'
     }
-    req = requests.get(url=target, headers=headers)
+    req = requests.get(url, headers=headers)
+    return BeautifulSoup(req.text, 'html.parser')
+
+def http_request_post(url, data = {}):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        'Cookie': 'Hm_lvt_094775c050f5046091b59ab43cd68c32=1687068391,1687182983; Hm_lvt_a71b1bc761fe3f26085e79b5fd6a7f71=1687068391,1687182983; __gads=ID=a485432064a7ee99-22a8a73bafe10077:T=1687068392:RT=1687183268:S=ALNI_MYSyj0mPUg7qdi_MyfckVec3BkmTQ; __gpi=UID=00000c5107fd5f03:T=1687068392:RT=1687183268:S=ALNI_MZ6HGkmFxDyIXOJQIQjOWI3eu3gdw; cf_chl_2=6e792d39cf13f69; cf_clearance=8hdFqlHJ4lSWmMWKw89jg25t6AJFoWblWb0ZalX28.c-1687192133-0-160; jieqiVisitTime=jieqiArticlesearchTime%3D1687192151; Hm_lpvt_094775c050f5046091b59ab43cd68c32=1687192152; Hm_lpvt_a71b1bc761fe3f26085e79b5fd6a7f71=1687192152'
+    }
+    req = requests.post(url, data, headers=headers)
     return BeautifulSoup(req.text, 'html.parser')
 
 # 列表过滤
